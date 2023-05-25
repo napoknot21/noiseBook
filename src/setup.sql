42e0985422f12680f10cae45aeb99f5f41fd80b7
Moys27 <ana.parres27@gmail.com>
2023-05-25 18:42:47 +0200

    add of association tables and relations

--- src/setup.sql
+++ src/setup.sql
@@ -1,5 +1,5 @@
 -- Sub entities (corresponding to 'Utilisateur')
-DROP TABLE IF EXISTS Utilisateur CASACADE;
+DROP TABLE IF EXISTS Utilisateur CASCADE;
 DROP TABLE IF EXISTS Groupe CASCADE;
 
 -- Sub entities (corresponding to 'Evenement')
@@ -37,12 +37,17 @@ DROP TABLE IF EXISTS Playlist CASCADE;
 -- Some relational associations
 DROP TABLE IF EXISTS concert CASCADE; -- group and event associated
 DROP TABLE IF EXISTS organise CASCADE; -- association and event associated
-
+DROP TABLE IF EXISTS interaction CASCADE;
+DROP TABLE IF EXISTS pl_belongs CASCADE ; -- The songs track belong to the PlayList
+DROP TABLE IF EXISTS relation_ta CASCADE;
+DROP TABLE IF EXISTS follow CASCADE;
+DROP TABLE IF EXISTS amitie CASCADE;
+--
 
 -- Users sub entities --
 
 CREATE TABLE Utilisateur (
-    id_user SERIAL INTEGER PRIMARY KEY NOT NULL,
+    id_user SERIAL PRIMARY KEY NOT NULL,
     name_assoc VARCHAR NOT NULL,
     email VARCHAR NOT NULL UNIQUE,
 );
@@ -55,7 +60,7 @@ CREATE TABLE Groupe (
 -- Place Entity --
 
 CREATE TABLE Lieu (
-    id_place SERIAL INTEGER PRIMARY KEY,
+    id_place SERIAL PRIMARY KEY,
     name_p VARCHAR NOT NULL,
     city_p VARCHAR NOT NULL,
     cp_p INTEGER NOT NULL -- code Postal
@@ -65,7 +70,7 @@ CREATE TABLE Lieu (
 -- Event sub Entities --
 
 CREATE TABLE Evenement_Passe (
-    id_event SERIAL INTEGER PRIMARY KEY,
+    id_event SERIAL PRIMARY KEY,
     id_p INTEGER NOT NULL,
     date_ep DATE NOT NULL,
     name_ep VARCHAR NOT NULL,
@@ -76,7 +81,7 @@ CREATE TABLE Evenement_Passe (
 );
 
 CREATE TABLE Evenement_Futur (
-    id_event SERIAL INTEGER PRIMARY KEY,
+    id_event SERIAL PRIMARY KEY,
     id_p INTEGER NOT NULL,
     date_ef DATE NOT NULL,
     name_ef VARCHAR NOT NULL,
@@ -87,7 +92,7 @@ CREATE TABLE Evenement_Futur (
 
 
 CREATE TABLE Morceau (
-    id_s SERIAL INTEGER PRIMARY KEY,
+    id_s SERIAL PRIMARY KEY,
     id_g INTEGER NOT NULL,
     album TEXT NOT NULL,
     n_order INTEGER NOT NULL,
@@ -98,17 +103,20 @@ CREATE TABLE Morceau (
 -- Playlist Entity --
 
 CREATE TABLE Playlist (
-    id_pl SERIAL INTEGER PRIMARY KEY,
+    id_pl SERIAL PRIMARY KEY,
     name_pl VARCHAR NOT NULL,
-    desc_pl VARCHAR,
+    desc_pl VARCHAR
 
 );
 
 
 -- Opinion sub entities --
 
+CREATE TABLE Avis (
+    id_avis SERIAL PRIMARY KEY
+);
+
 CREATE TABLE Avis_Groupe (
-    id_avis SERIAL INTEGER NOT NULL,
     id_user INTEGER NOT NULL,
     id_grp INTEGER NOT NULL
     commentary VARCHAR(50),
@@ -118,10 +126,9 @@ CREATE TABLE Avis_Groupe (
     FOREIGN KEY (id_user) REFERENCES Utilisateur (id_user) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (id_grp) REFERENCES Groupe (id_user) ON DELETE CASCADE ON UPDATE CASCADE,
     UNIQUE (id_user, id_grp) -- the user can only rate the band once 
-);
+) INHERITS (Avis);
 
 CREATE TABLE Avis_Morceau (
-    id_avis SERIAL INTEGER NOT NULL,
     id_user INTEGER NOT NULL,
     id_song INTEGER NOT NULL,
     commentary VARCHAR(50) NOT NULL,
@@ -131,10 +138,9 @@ CREATE TABLE Avis_Morceau (
     FOREIGN KEY (id_user) REFERENCES Utilisateur (id_user) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (id_song) REFERENCES Morceau (id_song) ON DELETE CASCADE ON UPDATE CASCADE,
     UNIQUE (id_user, id_song) -- the user can only rate the band once 
-);
+) INHERITS (Avis);
 
 CREATE TABLE Avis_Evenement (
-    id_avis SERIAL INTEGER NOT NULL,
     id_user INTEGER NOT NULL,
     id_event INTEGER NOT NULL,
     commentary VARCHAR(50),
@@ -144,10 +150,9 @@ CREATE TABLE Avis_Evenement (
     FOREIGN KEY (id_user) REFERENCES Utilisateur (id_user) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (id_event) REFERENCES Evenement_Passe (id_event) ON DELETE CASCADE ON UPDATE CASCADE,
     UNIQUE (id_user, id_event)
-);
+) INHERITS (Avis);
 
 CREATE TABLE Avis_Lieu (
-    id_avis SERIAL INTEGER NOT NULL,
     id_user INTEGER NOT NULL,
     id_place INTEGER NOT NULL,
     commentary VARCHAR(50) NOT NULL,
@@ -157,29 +162,32 @@ CREATE TABLE Avis_Lieu (
     FOREIGN KEY (id_user) REFERENCES Utilisateur (id_user) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (id_event) REFERENCES Lieu (id_place) ON DELETE CASCADE ON UPDATE CASCADE,
     UNIQUE (id_user, id_place)
-);
+) INHERITS (Avis);
 
 
 
 -- Tag Entities --
 
-CREATE TABLE Tag_General (
-    id_tag SERIAL INTEGER PRIMARY KEY NOT NULL,
-    text TEXT NOT NULL
+CREATE TABLE Tag (
+    id_tag SERIAL PRIMARY KEY NOT NULL,
 );
+
+CREATE TABLE Tag_General (
+    text TEXT NOT NULL,
+    UNIQUE(id_tag, text)
+) INHERITS (Tag);
 
 CREATE TABLE Tag_Lieu(
-    id_tp SERIAL INTEGER PRIMARY KEY NOT NULL,
     id_p INTEGER NOT NULL,
     FOREIGN KEY (id_p) REFERENCES Lieu (id_place) ON DELETE CASCADE ON UPDATE CASCADE,
-    UNIQUE (id_tp, id_p)
-);
+    UNIQUE (id_tag, id_p)
+) INHERITS (Tag);
 
 CREATE TABLE Tag_Groupe (
-    id_tg SERIAL INTEGER PRIMARY KEY NOT NULL,
     id_grp INTEGER NOT NULL,
-    FOREIGN KEY (id_grp) REFERENCES Groupe (id_user) ON DELETE CASCADE ON UPDATE CASCADE
-);
+    FOREIGN KEY (id_grp) REFERENCES Groupe (id_user) ON DELETE CASCADE ON UPDATE CASCADE,
+    UNIQUE (id_tag, id_grp)
+) INHERITS (Tag);
 
 
 
@@ -190,7 +198,6 @@ CREATE TABLE Genre (
     name_g VARCHAR PRIMARY KEY NOT NULL,
     UNIQUE (id_g, name_g)
 );
-
 
 
 -- -Associations- --
@@ -204,24 +211,63 @@ CREATE TABLE concert (
 );
 
 CREATE TABLE organise (
-    id_assoc INTEGER NOT NULL,
+    id_user INTEGER NOT NULL,
     id_event INTEGER NOT NULL,
-    PRIMARY KEY (id_event, id_lieu),
-    FOREIGN KEY (id_assoc) REFERENCES Association (id_assoc) ON DELETE CASCADE ON UPDATE CASCADE,
+    PRIMARY KEY (id_event, id_user),
+    FOREIGN KEY (id_user) REFERENCES Association (id_user) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (id_event) REFERENCES Evenement_Futur (id_event) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE status (
     id_p INTEGER NOT NULL,
-    id_event INTEGER NOT NULL,
+    id_user INTEGER NOT NULL,
     status_s TEXT IN ('INTERESSESTED', 'PARTICIPATE', 'NOT_INTERESTED'),
     PRIMARY KEY (id_p, id_event),
-    FOREIGN KEY (id_assoc) REFERENCES Association (id_assoc) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (id_user) REFERENCES Utilisateur (id_user) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (id_event) REFERENCES Evenement_Futur (id_event) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-/*
+
 CREATE TABLE follows (
-    id_
+    -- id_f SERIAL PRIMARY KEY,
+    id_userFollows INTEGER NOT NULL,
+    id_userFollowed INTEGER NOT NULL,
+    FOREIGN KEY (id_userFollows) REFERENCES Utilisateur (id_user) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (id_userFollowed) REFERENCES Utilisateur (id_user) ON DELETE CASCADE ON UPDATE CASCADE,
+    PRIMARY KEY (id_userFollows, id_userFollowed)
+);
+
+CREATE TABLE amitie (
+    id_userA INTEGER NOT NULL,
+    id_userB INTEGER NOT NULL,
+    FOREIGN KEY (id_userA) REFERENCES Utilisateur (id_user) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (id_userB) REFERENCES Utilisateur (id_user) ON DELETE CASCADE ON UPDATE CASCADE,
+    PRIMARY KEY (id_userA, id_userB)
+    -- CREATE SYMENTRIC KEY
+);
+
+CREATE TABLE pl_belongs (
+    id_s INTEGER NOT NULL,
+    id_pl INTEGER NOT NULL,
+    FOREIGN KEY (id_s) REFERENCES Morceau (id_s) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (id_pl) REFERENCES PlayList (id_pl) ON DELETE CASCADE ON UPDATE CASCADE
+    -- check for entering values
+    -- trigger
+
+);
+
+CREATE TABLE relation_ta (
+    id_t INTEGER NOT NULL,
+    id_avis INTEGER NOT NULL,
+    FOREIGN KEY (id_t) REFERENCES Tag (id_tag) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (id_avis) REFERENCES Avis (id_avis) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+CREATE TABLE derive_g (
+    id_gp INTEGER NOT NULL,
+    id_ge INTEGER NOT NULL,
+    FOREIGN KEY (id_gp) REFERENCES Genre (id_g) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (id_ge) REFERENCES Genre (id_g) ON DELETE CASCADE ON UPDATE CASCADE,
+    PRIMARY KEY (id_gp, id_ge)
 )
-*/
